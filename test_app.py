d35251ac3a551b9fe42e6efa22d17c435c571ece
wyl987 <wyl987@uowmail.edu.au>
2025-03-11 12:39:34 +1100

    Dash app test fixing

--- test_app.py
+++ test_app.py
@@ -1,26 +1,20 @@
-import pytest
-from dash import html
-from dash.testing.application_runners import import_app
+from dash_app import app
 
-@pytest.fixture
-def dash_app(dash_duo):
-    app = import_app("dash_app")  
-    dash_duo.start_server(app)
-    return dash_duo
-  
 # Test 1: The header is present.
-def test_header(dash_app):
-  dash_app.wait_for_element('h1', timeout=10)
-  assert dash_app.find_element('h1').text == 'Sales Data over time'
+def test_header(dash_duo):
+    dash_duo.start_server(app)
+    dash_duo.wait_for_element('h1', timeout=10).text == 'Sales Data over time'
 
 # Test 2: The visualisation is present.
-def test_visualisation(dash_app):
-  dash_app.wait_for_element('#visualization', timeout=10)
-  graph = dash_app.find_element('#visualization')
-  assert graph is not None, "Visualization (graph) is not present"
+def test_visualisation(dash_duo):
+    dash_duo.start_server(app)
+    dash_duo.wait_for_element('#visualization', timeout=10)
+    graph = dash_duo.find_element('#visualization')
+    assert graph is not None, "Visualization (graph) is not present"
 
-# Test 2: The region picker is present.
-def test_radio(dash_app):
-  dash_app.wait_for_element('#region-selector', timeout=10)
-  region_selector = dash_app.find_element('#region-selector')
-  assert region_selector is not None, "Region selector is not present"
+# Test 3: The region picker is present.
+def test_radio(dash_duo):
+    dash_duo.start_server(app)
+    dash_duo.wait_for_element('#region-selector', timeout=10)
+    region_selector = dash_duo.find_element('#region-selector')
+    assert region_selector is not None, "Region selector is not present"
